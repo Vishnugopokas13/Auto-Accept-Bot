55f98b6e35556b3e23b2dd67291cc822b6c3fd67
Vishnugopokas13 <137295050+Vishnugopokas13@users.noreply.github.com>
2023-09-15 16:32:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ async def op(_, m :Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://graph.org/file/811f42f960001051d9832.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__POWDERED BY: https://t.me/+LnXkSc9KJ_4xNjI9 __**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)    
+            await m.reply_photo("https://graph.org/file/811f42f960001051d9832.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__POWDERED BY: https://t.me/+BvBB0TPosmg3NTc1 __**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)    
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
                 [
