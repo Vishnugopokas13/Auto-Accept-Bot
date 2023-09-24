0c98ae11bcbfe503764c0453618b81239968ef2a
Vishnugopokas13 <137295050+Vishnugopokas13@users.noreply.github.com>
2023-09-24 17:49:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    API_ID = int(getenv("API_ID", "13305226"))
+    API_HASH = getenv("API_HASH", "8cde2475d6b0cb1162b89ebbac71a95d")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5088998402:AAGMWFsZfCSsW6IAMxuWuK2WGWPNqAQPQrM")
+    FSUB = getenv("FSUB", "wmteams")
+    CHID = int(getenv("CHID", "-1001651905628"))
+    SUDO = list(map(int, getenv("SUDO").split(1258310642)))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
